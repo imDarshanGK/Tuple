83aea5d64a44fbec4b90fbcebe9afc8dcf843e70
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-05-31 21:16:02 +0530

    tuple, list, set and dict.
    Built in functions which works on tuple, list, set and dict.

---
+++ Built in functions which works on tuple, list, set and dict..py
@@ -0,0 +1,9 @@
+#Built in functions which works on tuple, list, set and dict.
+marks=(34,54,65,78,22,87)
+print("minimum=",min(marks))
+print("maximum=",max(marks))
+print("sum=",sum(marks))
+print("length=",len(marks))
+print("sorted marks=",sorted(marks))
+del(marks)
+print("marks=",marks)
